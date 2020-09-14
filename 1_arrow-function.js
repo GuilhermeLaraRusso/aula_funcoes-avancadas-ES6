65f0cc474b25a4ec411173efab0c3f5eef8508ac
Guilherme <guilhermelrusso@gmail.com>
2020-09-14 09:36:24 -0300

    Initial commit

---
+++ 1_arrow-function.js
@@ -0,0 +1,16 @@
+/** Arrow Functions */
+
+// No caso de uma expressão, pode-se emitir o return
+
+var sum = (a, b) => a + b;      
+
+console.log(sum(5, 15));
+
+
+
+
+// Nos outros casos, coloca-se a 'chaves' {}
+
+var sum = (a, b) => {
+
+}
